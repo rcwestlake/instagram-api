49e01ca34571e8cf992378d4467cadec3420227e
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-21 16:17:59 -0700

    add more interactive.. button press

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    console.log('js check');
+    $('#searchPhotos').click(function() {
+        $(this).fadeOut('fast');
+        $('#intro-header').fadeOut('fast');
+        $('#photoHeader').fadeIn('fast');
+        
+        
+        
+    })
+    
+});
