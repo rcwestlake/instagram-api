85e95fc6691d01602d0efae91368bd5bd6999466
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-22 20:48:29 -0700

    code that works

--- js/app.js
+++ js/app.js
@@ -1,12 +1,49 @@
 $(document).ready(function() {
     console.log('js check');
+    
+    /*press button to show photos*/
+    
     $('#searchPhotos').click(function() {
         $(this).fadeOut('fast');
         $('#intro-header').fadeOut('fast');
         $('#photoHeader').fadeIn('fast');
-        
-        
-        
+        getData("#wrapper1");
+        getData("#wrapper2");
+        getData("#wrapper3");
     })
+    
+    
+    
+    var getData = function(div) {
+        $(function() {
+
+            $.ajax({
+                type: "GET",
+                dataType: "jsonp",
+                cache: false,
+                url: "https://api.instagram.com/v1/media/popular?client_id=85fb603fb11f45a385651e0f2e209bb1",
+                success: function(data) {
+                    photoData = data;
+                    main(data, div);
+                }
+            });
+        });
+    }
+    
+    
+    var main = function(data, div) {
+        var html = "";
+        for (var i = 0; i < data.data.length; i++)
+        {
+            console.log(data.data[i].images.thumbnail.url); 
+    
+    		html += '<div class="photoGrid"><img src="' + data.data[i].images.thumbnail.url + '"></div>';
+    	
+        }
+        $(div).html(html);
+    }
+    
+    
+   
     
 });
